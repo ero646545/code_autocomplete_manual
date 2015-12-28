655bb425403ca35aea57b7fced6a5f7b0ce78953
Jacques Lucke <mail@jlucke.com>
2015-12-28 01:40:23 +0100

    conf file

---
+++ docs/conf.py
@@ -0,0 +1,46 @@
+extensions = [
+    'sphinx.ext.autodoc',
+]
+
+templates_path = ['ytemplates']
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = u'Code Autocomplete'
+copyright = u'2015, Jacques Lucke'
+
+version = '0.0.1'
+release = '0.0.1'
+
+exclude_patterns = []
+
+pygments_style = 'sphinx'
+
+html_theme = 'default'
+
+html_static_path = ['ystatic']
+
+htmlhelp_basename = 'CodeAutocompleteManual'
+
+
+latex_elements = {}
+latex_documents = [
+  ('index', 'CodeAutocomplete.tex', u'Code Autocomplete Manual',
+   u'Jacques Lucke', 'manual'),
+]
+
+man_pages = [
+    ('index', 'codeautocomplete', u'Code Autocomplete Manual',
+     [u'Jacques Lucke'], 1)
+]
+texinfo_documents = [
+  ('index', 'CodeAutocomplete', u'Code Autocomplete Manual',
+   u'Jacques Lucke', 'CodeAutocomplete', 'Unleash the power of scripting in Blender',
+   'Miscellaneous'),
+]
+
+epub_title = u'Code Autocomplete'
+epub_author = u'Jacques Lucke'
+epub_publisher = u'Jacques Lucke'
+epub_copyright = u'2015, Jacques Lucke'
+epub_exclude_files = ['search.html']
---
+++ docs/index.rst
@@ -0,0 +1,11 @@
+**********************
+Code Autocomplete Manual
+**********************
+
+Welcome to the official manual of the **`Code Autocomplete`_** addon.
+
+
+.. toctree::
+   :maxdepth: 2
+
+   .. _Code Autocomplete: https://github.com/JacquesLucke/code_autocomplete
